a69ffcb81370d00c9116623b106978a64232e1e2
jpball2 <jpball2@asu.edu>
2025-02-19 13:43:45 -0700

    Update uber_pickups.py

--- uber_pickups.py
+++ uber_pickups.py
@@ -32,6 +32,7 @@ hist_values = np.histogram(
             data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
 
-
-st.subheader('Map of all pickups')
-st.map(data)
+hour_to_filter = 17
+filtered_data = [data[DATE_COLUMN].dt.hour == hour_to_filter]
+st.subheader(f'Map of all pickups at {hour to filter}:00')
+st.map(filtered_data)
